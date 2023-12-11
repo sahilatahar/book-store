c62c082f7372e2e3c4473b47c3acc9b4a5c63ed5
Sahil Atahar <sahilatahar@gmail.com>
2023-12-11 12:55:17 +0530

    adding cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://sahilatahar-book-store-api.vercel.app/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 
 connectDB();
 
